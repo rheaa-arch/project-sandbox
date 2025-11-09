97e1d6c206b6a8b68050094fcc2f445055af5c09
rheaa-arch <drea2840@gmail.com>
2025-11-09 23:40:14 +0700

    Create lesson_01_select.sql

---
+++ 01_sqlbolt_exercises/lesson_01_select.sql
@@ -0,0 +1,9 @@
+-- Lesson 01: SELECT basics
+-- Practice queries from SQLBolt
+-- Table used: movies (columns: id, title, director, year, length_minutes)
+
+SELECT title FROM movies;
+SELECT director FROM movies;
+SELECT title, director FROM movies;
+SELECT title, year FROM movies;
+SELECT * FROM movies;
