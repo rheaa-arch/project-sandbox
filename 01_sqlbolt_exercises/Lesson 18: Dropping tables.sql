99f8210fb8e88a7d71ecbed1d135e700c1efb41f
rheaa-arch <drea2840@gmail.com>
2025-11-10 15:52:04 +0700

    Create Lesson 18: Dropping tables.sql

---
+++ 01_sqlbolt_exercises/Lesson 18: Dropping tables.sql
@@ -0,0 +1,7 @@
+-- Lesson 18: DROP TABLE basics
+-- Practice queries from SQLBolt
+-- Table used: movies (columns: id, title, director, year, length_minutes)
+-- Table used: boxoffice (columns: movie_id,	rating,	domestic_sales,	international_sales)
+
+DROP TABLE IF EXISTS Movies;
+DROP TABLE IF EXISTS Boxoffice;
