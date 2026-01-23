331aea6c703ce2f700f5b93a0b26cfc7ce7c04a8
rheaa-arch <drea2840@gmail.com>
2026-01-24 01:01:52 +0700

    Update and rename Difficulty - Easy to Difficulty - Easy.sql

--- sql-practice/Difficulty - Easy.sql
+++ sql-practice/Difficulty - Easy.sql
@@ -1,5 +1,5 @@
 -- Show first name, last name, and gender of patients whose gender is 'M'
-SELECT first_name, last_name, gender FROM patients
+SELECT first_name, last_name, gender FROM patients 
 where gender is 'M';
 
 -- Show first name and last name of patients who does not have allergies. (null)
@@ -40,3 +40,31 @@ where height = (
   select max(height)
   from patients
   );
+
+-- Show all columns for patients who have one of the following patient_ids: 1,45,534,879,1000
+SELECT 
+	patient_id,
+	first_name,
+    last_name,
+    gender,
+    birth_date,
+    city,
+    province_id,
+    allergies,
+    height,
+    weight
+from patients
+where patient_id in (1,45,534,879,1000);
+
+-- Show the total number of admissions
+SELECT count(*) as total_admission from admissions;
+
+-- Show all the columns from admissions where the patient was admitted and discharged on the same day.
+select 
+	patient_id,
+    admission_date,
+    discharge_date,
+    diagnosis,
+    attending_doctor_id
+from admissions
+where admission_date = discharge_date;
