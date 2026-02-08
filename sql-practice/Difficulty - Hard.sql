21dc7ce5db9ae89b4d8990f2c4e66c3c2b25a833
rheaa-arch <drea2840@gmail.com>
2026-02-08 22:30:16 +0700

    Update Difficulty - Hard.sql

--- sql-practice/Difficulty - Hard.sql
+++ sql-practice/Difficulty - Hard.sql
@@ -55,3 +55,14 @@ on p.patient_id = a.patient_id;
 
 -- Each admission costs $50 for patients without insurance, and $10 for patients with insurance. All patients with an even patient_id have insurance.
 -- Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. Add up the admission_total cost for each has_insurance group.
+select has_insurance,sum(admission_cost) as admission_total
+from
+(
+   select patient_id,
+   case when patient_id % 2 = 0 then 'Yes' else 'No' end as has_insurance,
+   case when patient_id % 2 = 0 then 10 else 50 end as admission_cost
+   from admissions
+)
+group by has_insurance;
+
+-- Show the provinces that has more patients identified as 'M' than 'F'. Must only show full province_name
